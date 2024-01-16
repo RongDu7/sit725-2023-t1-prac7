25536ead0a295fe87a1c6bab2da7693898e197c1
RongDu7 <151746090+RongDu7@users.noreply.github.com>
2024-01-16 13:57:31 +1100

    sit725-2023-t1-prac7

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-const express= require("express");
-const res = require("express/lib/response");
-const app= express();
+let express = require('express');
+let app = express();
+let port = process.env.port || 3000;
+require('./dbConnection');
+let router = require('./routers/router');
+const { Socket } = require('socket.io');
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
-app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
-    var firstNumber = parseInt(req.params.firstNumber) 
-    var secondNumber = parseInt(req.params.secondNumber)
-    var result = firstNumber + secondNumber || null
-    if(result == null) {
-      res.json({result: result, statusCode: 400}).status(400)
-    }
-    else { res.json({result: result, statusCode: 200}).status(200) } 
-  })
+app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use('/api/cat',router);
 
+io.on('connection',(socket)=>{
+    console.log('user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 
-const port=8080;
-app.listen(port,()=> {
-    console.log("hello i'm listening to port "+port);
-})
+    setInterval(()=>{
+        x=parseInt(Math.random()*10);
+        socket.emit('number', x);
+        console.log('Emmiting Number '+x);
+    }, 1000)
+});
+
+http.listen(port, ()=>{
+    console.log('express server started');
+});
